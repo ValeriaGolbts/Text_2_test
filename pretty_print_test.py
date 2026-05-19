492070602b16135ac6f4267175e667f6d67ff6ce
ValeriaGolbts <golubez2002@gmail.com>
2026-05-19 19:22:06 +0700

    Update pretty_print_test.py

--- pretty_print_test.py
+++ pretty_print_test.py
@@ -1,7 +1,7 @@
 # pretty_print_test.py
 import json
 
-with open('test_result_20260224_184008.json', 'r', encoding='utf-8') as f:
+with open('test_result_S1_random_lecture_20260519_191606.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 print(json.dumps(data, ensure_ascii=False, indent=2))
