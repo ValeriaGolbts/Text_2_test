f44d7acf4939c9808d01a390d236f8458a098592
ValeriaGolbts <golubez2002@gmail.com>
2026-05-25 02:23:23 +0700

    Update json_to_pdf_1.py

--- json_to_pdf_1.py
+++ json_to_pdf_1.py
@@ -300,7 +300,7 @@ def json_to_pdf_questions_only(json_path, pdf_path):
             pass
 
 if __name__ == "__main__":
-    input_json = "test_result_S1_random_lecture_20260519_191606.json"
+    input_json = "res_fin.json"
     output_pdf = "questions_output.pdf"
     
     if os.path.exists(input_json):
